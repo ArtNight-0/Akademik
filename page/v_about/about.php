c81cb4996536e4463b8f4399b3a06073d40ff1db
sulthonadam <justdamz20@gmail.com>
2022-11-01 23:08:01 +0700

    newcode1

--- page/v_about/about.php
+++ page/v_about/about.php
@@ -9,38 +9,74 @@
 </head>
 <body>
 <section class="project">
-      <div class="container">
-        <div class="row mx-auto">
-          <div class="title">
-            <h2>my <a style="color: #502e9c; font-style: none">Bruh</a></h2>
-            <hr class="mx-auto" />
-          </div>
-          <div class="project-card text-center justify-content-between">
-            <div class="card" style="width: 24rem">
-              <img src="../img/prismme.png" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">mendesign logo <a style="color: #502e9c; font-style: none"> Bruh</a></h5>
-                <p class="card-text">Saya membuat sebuah logo start-up bernama “Prismme”. Metaphora logo ini terdiri dari dua bagian, yaitu segitiga dan prisma yang di design sedemikian rupa hingga mendapatkan hasil konsep tersebut.</p>
-              </div>
-            </div>
-            <div class="card" style="width: 24rem">
-              <img src="../img/Group 231.png" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">design <a style="color: #502e9c; font-style: none"> UI/UX</a> sebuah website<a style="color: #502e9c; font-style: none"> hotel</a></h5>
-                <p class="card-text">UI/UX Hotel ini saya buat dari permintaan sebuah klien yang ingin membuat sebuah website hotel dimana klien ingin terdapat fitur-fitur yang sederhana dan mudah di akses oleh calon penggunanya.</p>
-              </div>
-            </div>
-            <div class="card" style="width: 24rem">
-              <img src="../img/poster.png" class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">design<a style="color: #502e9c; font-style: none"> poster</a> Bruh</h5>
-                <p class="card-text">Poster yang saya buat berdasarkan tema Covid-19 ini di design untuk mengikuti sebuah kompetisi yang diadakan oleh kampus UPI.</p>
-              </div>
-            </div>
-          </div>
+    <div class="container">
+        <div class="title">
+            <h2>Anggota Kelompok 6</h2>
         </div>
-      </div>
-</section>
+    <div class="row">
+        <div class="col-4">
+        <div class="card" style="width: 24rem;">
+            <img src="../../img/cat.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title">Agus Rohendi</h5>
+                <h6>21212121</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        </div>
+        <div class="col-4">
+        <div class="card" style="width: 24rem;">
+            <img src="../../img/cat.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title">Ikhsan Muhammad A.R</h5>
+                <h6>21212121</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        </div>
+        <div class="col-4">
+        <div class="card" style="width: 24rem;">
+            <img src="../../img/cat.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title">Rafli Hendarsyah</h5>
+                <h6>21212121</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        </div>
+        <div class="col-4">
+        <div class="card" style="width: 24rem;">
+            <img src="../../img/cat.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title">Rafly Ardiansyah</h5>
+                <h6>21212121</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        </div>
+        <div class="col-4">
+        <div class="card" style="width: 24rem;">
+            <img src="../../img/cat.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title">Muhammad Iqbal Bahktiar</h5>
+                <h6>21212121</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        </div>
+        <div class="col-4">
+        <div class="card" style="width: 24rem;">
+            <img src="../../img/cat.jpg" class="card-img-top" alt="...">
+            <div class="card-body text-center">
+                <h5 class="card-title">Sulthon Adam Maulana</h5>
+                <h6>21212121</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+    </section>
 
 </body>
 </html>
--- page/v_data_mahasiswa/index.php
+++ page/v_data_mahasiswa/index.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/stylee.css">
     <title>Document</title>
 </head>
 <body>
+     <div class="container title mt-4 text-center">
+    <h2>Tabel Mahasiswa</h2>
+  </div>
     <div class="halaman col-8 mx-auto">
-        <a class="btn btn-sm btn-primary mx-3 mt-3 mb-2" href="../v_data_mahasiswa/tambah.php">+Tambah Data</a>
+        <a class="btn btn-sm btn-primary mt-4 mb-3" href="../v_data_mahasiswa/tambah.php">+Tambah Data</a>
     <table class="table table-striped">
         <tr>
         <th>NO</th>
